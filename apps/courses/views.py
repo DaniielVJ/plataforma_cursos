760889417aa05c4eb70e2254f7ad37e36fd33145
Daniel Valdebenito <danielvaldebenito25@gmail.com>
2025-10-08 19:24:27 -0300

    añadir contenido dinamico para cada curso en el template utilizando una base de datos fictisia en una lista

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -2,12 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# Datos ficticios para simular una base de datos
+
+doomy_data = [
+    {
+        "id": 1, "name": "django", "level": "beginner", "rating": 5.0, 
+        "title": "Three-month Course to Learn the Basics of Python and StartCoding.",
+        "author": "Alison Walsh JIIJIIJIJIJIJIJIJI",
+     },
+    {
+        
+    }
+    
+]
+
 
 def courses_list(request):
-    return render(request, 'courses/courses.html')
+    # Version para probar antes de irme
+    return render(request, 'courses/courses.html', {'course': doomy_data[0]})
 
 
-def course_detail(request):
+    # Original a usar luego
+    return render(request, 'courses/courses.html', {'list_courses': doomy_data})
+
+
+def course_detail(request, name:str = ""):
     return render(request, 'courses/course_detail.html')
 
 

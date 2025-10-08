5596b1a4af3977f2e32e0d87622da5fd6d3b83d7
Daniel Valdebenito <danielvaldebenito25@gmail.com>
2025-10-08 17:04:09 -0300

    Añadir templates y archivos estaticos globales y app

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,10 +116,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
+
+# Registramos los archivos estaticos globales
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

a14dabc67e7ec3c32be234419c7687db6deb917a
Daniel Valdebenito <danielvaldebenito25@gmail.com>
2025-10-10 19:20:01 -0300

    Definir urls, templates y views de nuestra app dashboard

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
 
 
 
 urlpatterns = [
-    
+    # raiz de dashboard, es decir usa la ruta definida en el proyecto para dashboard
+    path('', views.index, name='dashboard_index') # responde a -> /dashboard
+    # <- url encargada de cargar el index de dashboard
 ]
